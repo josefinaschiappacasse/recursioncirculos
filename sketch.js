504439bed79588d2fa0180cee25e7c789f3ea339
josefinaschiappacasse <69818086+josefinaschiappacasse@users.noreply.github.com>
2020-09-25 09:36:17 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
 
 
+
+let s1, F; 
+
 function setup() {
   createCanvas(500, 400);
+  s1 = createSlider(0,400);
 }
 
 function draw() {
   background(0);
+  F = s1.value(),
   stroke("white");
   noFill();
   drawCircle(30, 370, 30);
@@ -15,8 +20,8 @@ function drawCircle(x, y, d) {
   ellipse(x, y, d);
 
   if (x < width) {
-    drawCircle(x + 40, y, d* .67);
-    drawCircle(x + 40, y - 30, d);
+    drawCircle(x + 30, y, d* .67);
+    drawCircle(x + 30, y - F, d);
    
 
   }
